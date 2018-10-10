bcfe58f17f31a5ee8c3385bd01ff376c3410af01
1521350289@qq.com <1521350289@qq.com>
2018-10-10 13:50:55 +0800

    .

---
+++ application/api/controller/v2/Banner.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hasee
+ * Date: 2018/10/5
+ * Time: 下午 5:57
+ */
+
+namespace app\api\controller\v2;
+
+
+use app\api\model\Banner as BannerModel;
+use app\api\validate\IDMustBePostiveInt;
+use app\lib\exception\BannerMissException;
+
+class Banner
+{
+    /**
+     * 获取指定id的banner信息
+     * @url /banner/:id
+     * @http GET
+     * @id banner的id号
+     */
+    public function getBanner($id)
+    {
+        return 'this is v2 version';
+    }
+}
