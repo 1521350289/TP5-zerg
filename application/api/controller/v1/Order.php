f71e203572c3b4ea176d6e6c76639b6c91ba82e3
1521350289@qq.com <1521350289@qq.com>
2018-10-27 20:21:43 +0800

    .

--- application/api/controller/v1/Order.php
+++ application/api/controller/v1/Order.php
@@ -12,12 +12,14 @@ use app\api\controller\BaseController;
 use app\api\service\Token as TokenService;
 
 
+use app\api\validate\IDMustBePostiveInt;
 use app\api\validate\OrderPlace;
 use app\api\service\Order as OrderService;
 use app\api\validate\PagingParameter;
 use app\api\model\Order as OrderModel;
 use app\lib\enum\ScopeEnum;
 use app\lib\exception\ForbiddenException;
+use app\lib\exception\OrderException;
 use app\lib\exception\TokenException;
 use think\Controller;
 
@@ -35,9 +37,10 @@ class Order extends BaseController
 
     protected $beforeActionList = [
         'checkExclusiveScope' => ['only'=>'placeOrder'],
-        'checkPrimaryScop'=>['only'=>'getSummaryByUser'],
+        'checkPrimaryScope'=>['only'=>'getDetail,getSummaryByUser'],
     ];
 
+    //订单 列表
     public function getSummaryByUser($page = 1,$size = 15)
     {
         (new PagingParameter())->goCheck();
@@ -55,6 +58,17 @@ class Order extends BaseController
             'data'=>$data,
             'current_page'=>$pagingOrders->getCurrentPage()
         ];
+    }
+    
+    //订单详情
+    public function getDetail($id)
+    {
+        (new IDMustBePostiveInt())->goCheck();
+        $orderDetail = OrderModel::get($id);
+        if (!$orderDetail){
+            throw new OrderException();
+        }
+        return $orderDetail->hidden(['prepay_id']);
     }
 
     //订单 创建
